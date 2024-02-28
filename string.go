3f3af8cf5dbf8cfb506ffb5c50b7d17ee4ce8e54
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-02-28 14:57:07 +0800

    Learning Couples Golang materials

---
+++ string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Diqi"))            //Space include
+	fmt.Println("Muhammad Shiddiqi"[0]) //yang diambil adalah byte nya, jadi byte dari karakter pertama (m) adalah 77
+	fmt.Println("Muhammad Shiddiqi Musyaffa'"[1])
+
+}
---
+++ variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Muhammad Shiddiqi Musyaffa"
+	fmt.Println(name)
+
+	name = "Diqi" //akan menimpa nilai sebelumnya dari variabel yang di panggil
+	fmt.Println(name)
+}
