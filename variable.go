948045a7c40f28e4e21dd20b176d937bdd0ef1b9
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-02-28 15:06:23 +0800

    Learning Variable

--- variable.go
+++ variable.go
@@ -10,4 +10,21 @@ func main() {
 
 	name = "Diqi" //akan menimpa nilai sebelumnya dari variabel yang di panggil
 	fmt.Println(name)
+
+	//=====================================================================================
+	var name2 = "Anjay Mabar" //golang akan secara otomatis akan memasukkan tipe datanya, yaitu string
+	fmt.Println(name2)
+
+	//======================================================================================
+	name3 := "Kocak Gaming" //menggunakan := hanya pada awal inisialisasi sebuah variabel baru
+	fmt.Println(name3)
+
+	//======================================================================================
+	var (
+		firstName  = "Muhammad"
+		middleName = "Shiddiqi"
+		LastName   = "Musyaffa'"
+	)
+
+	fmt.Println(firstName, middleName, LastName)
 }
