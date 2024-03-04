5372d7a6b75895cb85202c509fb6811c64337758
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-03-04 11:46:16 +0800

    Adding Stuffs

--- mathoperator.go
+++ mathoperator.go
@@ -7,4 +7,12 @@ func main()  {
 	var b = 10
 	var c = a + b
 	fmt.Println(c)
+	
+
+	// =========================================
+	// Augmented Assignments
+	var i = 10;
+	i += 10;
+	i += 5;
+	fmt.Println(i)
 }
